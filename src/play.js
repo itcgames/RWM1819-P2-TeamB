01144d88912ed250e0113b734c1a83701b641cc2
John O'Grady <johndj.og@gmail.com>
2018-11-29 09:36:58 +0000

    testing interactable collision

--- src/play.js
+++ src/play.js
@@ -8,7 +8,8 @@ class Play
 
       this.collisionManager = new CollisionManager();
       this.player = new Player();
-      this.interactableTest = new Interactable(400, 50, 50, 50, "test",'magenta', {minX: 400, minY: 50, maxX: 500, maxY: 50});
+      this.interactableTest = new Interactable(400, 300, 100, 50, "test",'magenta', {minX: 400, minY: 300, maxX: 500, maxY: 300});
+
       this.squares = [];
       for(var i = 1; i < 5; i ++)
       {
@@ -19,7 +20,7 @@ class Play
       this.squares.push(new BoxCollider(new Vector2(0, 300), 200, 100, ['ground']));
       this.squares.push(new BoxCollider(new Vector2(1000, 300), 200, 100, ['ground']));
       this.player.init();
-
+      this.collisionManager.addBoxCollider(this.interactableTest.getCollider());
       this.collisionManager.addCircleCollider(this.player.circle);
       for(var i = 0; i < this.squares.length; i ++)
         {
@@ -30,18 +31,19 @@ class Play
   update() {
       //  Update game objects here.
      this.collisionResults = this.collisionManager.checkCircleAndBoxColliderArray();
-        for(var i = 0; i < this.collisionResults['Array1'].length; i++){
-            if(this.collisionResults['Array1'][i][CollisionManager.IndexOfElement(this.collisionManager.circleColliderArray, this.player.circle)] == true){
-                this.player.handleCollision(this.collisionManager.boxColliderArray[i]);
-            }
+     for(var i = 0; i < this.collisionResults['Array1'].length; i++){
+        if(this.collisionResults['Array1'][i][CollisionManager.IndexOfElement(this.collisionManager.circleColliderArray, this.player.circle)] == true){
+            this.player.handleCollision(this.collisionManager.boxColliderArray[i]);
+            
+            if(this.collisionResults['Array1'][i][CollisionManager.CollidedWithTag());
         }
+     }
 
       this.player.update();
   }
 
 
   render(ctx) {
-      this.interactableTest.draw(ctx);
       this.collisionManager.render(ctx);
   }
 }
